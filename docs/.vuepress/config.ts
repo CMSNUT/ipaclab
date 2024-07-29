b87a23b2ff013bac653983ebd86afe8e11883e9f
CMSNUT <dhxia@snut.edu.cn>
2024-07-30 04:39:16 +0800

    deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { plumeTheme } from 'vuepress-theme-plume'
 import * as path from 'node:path'
+import process from 'node:process'
 
 export default defineUserConfig({
   base: '/ipaclab/',
@@ -12,11 +13,12 @@ export default defineUserConfig({
   },
 
   theme: plumeTheme({
+    hostname: process.env.SITE_HOST || 'https://cmsnut.github.io/ipaclab/',
     plugins: {
       shiki: {
         theme: { light: 'vitesse-light', dark: 'vitesse-dark' },
-        languages: ['javascript', 'typescript', 'python', 'r', 'matlab', 'vue', 'md', 'sql', 'tex', 'bibtex'],
-        twoslash: true,
+        languages: ['javascript', 'typescript', 'python', 'r', 'matlab', 'vue', 'md', 'sql', 'tex', 'bibtex','bash', 'sh'],
+        // twoslash: true,
       },
       markdownEnhance: {
         hint: true, // 提示容器
@@ -40,17 +42,17 @@ export default defineUserConfig({
         imgMark: true, // 浅色/深色 图片标记
         imgSize: true, // 图片尺寸支持
         obsidianImgSize: true, // obsidian 图片尺寸支持
-        mathjax: true, //  Math Jax 数学公式 语法支持
-        chart: true, // 图表支持
-        echarts: true, // ECharts 图表支持
-        flowchart: true, // 流程图支持
-        markmap: true, // Markmap 图表支持
+        // mathjax: true, //  Math Jax 数学公式 语法支持, katex具有优先
+        // chart: true, // 图表支持
+        // echarts: true, // ECharts 图表支持
+        // flowchart: true, // 流程图支持
+        markmap: true, // Markmap 图表支持(思维导图)
         // stylize: true, // 对行内语法进行样式化以创建代码片段
-        playground: true, // 交互演示
-        kotlinPlayground: true, // Kotlin 交互演示
-        vuePlayground: true, // Vue 交互演示
-        sandpack: true, // sandpack 交互演示
-        demo: true, // 代码案例
+        // playground: true, // 交互演示
+        // kotlinPlayground: true, // Kotlin 交互演示
+        // vuePlayground: true, // Vue 交互演示
+        // sandpack: true, // sandpack 交互演示
+        // demo: true, // 代码案例
         revealJs: true, // 幻灯片支持
       },
       markdownPower: {
@@ -59,12 +61,12 @@ export default defineUserConfig({
         // icons: true, // :[collect:name]:   内联 iconify 图标
         bilibili: true, // @[bilibili](bvid)  嵌入 bilibili 视频
         youtube: true, // @[youtube](id)  嵌入 youtube 视频
-        codepen: true, // @[codepen](user/slash)  嵌入 codepen
-        replit: true, // @[replit](user/repl-name)  嵌入 Replit
-        codeSandbox: true, // @[codesandbox](id)  嵌入 CodeSandbox
-        jsfiddle: true, // @[jsfiddle](id)  嵌入 jsfiddle
+        // codepen: true, // @[codepen](user/slash)  嵌入 codepen
+        // replit: true, // @[replit](user/repl-name)  嵌入 Replit
+        // codeSandbox: true, // @[codesandbox](id)  嵌入 CodeSandbox
+        // jsfiddle: true, // @[jsfiddle](id)  嵌入 jsfiddle
         // caniuse: true, // @[caniuse](feature)  嵌入 caniuse
-        repl: true, // :::go-repl   :::kotlin-repl  :::rust-repl
+        // repl: true, // :::go-repl   :::kotlin-repl  :::rust-repl
         plot: true, // <Plot>悬停时可见</Plot>  <Plot trigger="click">点击时可见</Plot>
       },
       // watermark: {
